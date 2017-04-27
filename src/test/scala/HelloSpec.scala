b19e74b7156e20349f66e39ada695dda60b634ed
Roland Tritsch <roland.tritsch@gonitro.com>
2017-04-27 15:10:34 +0100

    Reduce logging. Remove INFOs from test output

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -6,8 +6,12 @@ import org.scalatest.FlatSpec
 class HelloSpec extends FlatSpec with BeforeAndAfterAll {
   import Hello._
 
+  override def afterAll(): Unit = {
+    spark.close()
+  }
+
   "buildWordSizeHistogram(String)" should "return a/the histogram of word sizes" in {
-    val histo = buildWordSizeHistogram("1 12 12 123 123 123 ")
+    val histo = buildWordSizeHistogram("1 12 12 123 123 123")
     histo.show()
     assert(histo.count() === 3)
   }
